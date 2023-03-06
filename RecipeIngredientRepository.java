6396360fb083e162782e5a6dd4f26ea9a865e984
Alagandula <madhu.alagandula@cgi.com>
2023-03-06 23:57:48 +0100

    Deleted Unwanted files

--- RecipeIngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package nl.abnamro.intake.assesement.recipe.data.repository;
-
-import nl.abnamro.intake.assesement.recipe.data.dto.RecipeIngredientDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredientDto, Integer> {
-}
